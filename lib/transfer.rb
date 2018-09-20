fc8c38e90c47a0682a650e9c657b2df387b4cf2c
zhifeng ou <zhifengou@hotmail.com>
2018-09-20 20:43:55 +0000

    bank_account

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,35 @@
 class Transfer
   # your code here
+  attr_accessor :sender, :receiver, :amount, :status
+
+  def initialize(sender, receiver, amount=50, status="pending")
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = status
+  end
+
+  def valid?
+    @receiver.valid? && @sender.valid? # @receiver and @sender are BankAccount instances
+  end
+
+  def execute_transaction
+    if @status == "pending" && @sender.balance > amount
+      @sender.balance -= amount
+      @receiver.balance += amount
+      @status = "complete"
+    else
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+    end
+  end
+
+  def reverse_transfer
+    if @status == "complete"
+      @sender.balance += amount
+      @receiver.balance -= amount
+      @status = "reversed"
+    end
+  end
+
 end
